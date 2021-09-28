ecfea0bdc350e0a59c1a8745000d405ae7a51c50
matheustorsoni <matheus_tcampos@hotmail.com>
2021-09-28 15:14:16 -0300

    teste node

---
+++ teste.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+router.get('/', function(req,res){
+res.sendFile(path.join(__dirname+'/teste2.html'));
+
+})
+
+router.get('/sobre', function(req,res){
+    res.sendFile(path.join(__dirname+'/sobre.html'));
+    
+    })
+
+app.use('/',router);
+app.listen(process.env.port || 3000);
+
+console.log("teste 1");
